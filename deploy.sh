6cfd475e4e3baa72180fa0ffb7cdcce0351ae3ec
Maxwell Forbes <m.b.forbes@gmail.com>
2014-06-25 11:07:36 -0700

    Change deploy script to copy ignored files back into master, otherwise are removed after checking out master again.

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,8 @@ git push origin master
 git checkout deploy
 git merge master
 git push heroku deploy:master
+cp exp_url ../
+cp abilities_url ../
 git checkout master
+mv ../exp_url .
+mv ../abilities_url .
