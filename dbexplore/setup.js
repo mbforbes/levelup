0b91b83b5155929b8977752c90bf52ad227c3952
Maxwell Forbes <m.b.forbes@gmail.com>
2014-08-20 18:48:42 -0700

    Switch callback-spaghetti to async waterfall. Woah.

--- dbexplore/setup.js
+++ dbexplore/setup.js
@@ -1,77 +1,75 @@
 var pg = require('pg');
 var http = require('http');
 var pgconfig = require('../config/database.js');
+var async = require('async');
 
 // make a client
 var client = new pg.Client(pgconfig.url);
 client.connect();
 
-// functions
-var maybeCreateTable = function(client, email) {
-	// create table maybe
-	console.log('Maybe creating table.');
-	client.query("                       \
-	CREATE TABLE IF NOT EXISTS player (  \
-		email  varchar(255),             \
-		id     serial                    \
-	);                                   \
-	", function(err, result) {
-		if (err) {
-			console.error("Couldn't create player table.");
-		} else {
-			maybeAddUser(client, email);
-		}
-	});
-};
-
-var printUsers = function(client) {
-	// get data
-	console.log('Getting data.');
-	var query = client.query("SELECT * FROM player");
-
-	// report
-	query.on('row', function(row) {
-		console.log(row);
-	});
-
-	// finish
-	query.on('end', function() {
-		client.end();
-	});
-};
-
-var maybeAddUser = function(client, email) {
-	// get whether user email exists.
-	var query = client.query(
-		'SELECT * from player WHERE email=$1',
-		[email],
-		function(err, result) {
-			if (err) {
-				console.error("Couldn't query player table.");
-			} else if (result.rows.length > 0) {
-				console.error("User " + email + " already exists.");
-				printUsers(client);
-			} else {
-				addUser(client, email);
-			}
-		}
-	);
-};
-
-var addUser = function(client, email) {
-	// insert data
-	var query = client.query(
-		"INSERT INTO player (email) VALUES ($1)",
-		[email],
-		function (err, result) {
-			if (err) {
-				console.error("Error adding player to table.");
-			}
-			printUsers(client);
-		}
-	);
-};
-
-// 'real' code
+// settings
 var email = 'm.b.forbes@gmail.com';
-maybeCreateTable(client, email);
+
+// run queries in order
+async.waterfall([
+
+	// create table maybe
+	function(callback) {
+		console.log('Maybe creating table.');
+		client.query("                           \
+			CREATE TABLE IF NOT EXISTS player (  \
+				email  varchar(255),             \
+				id     serial                    \
+			);",
+			callback
+		);
+	},
+
+	// check whether user exists
+	function(prevResult, callback) {
+		console.log('Getting user existence.');
+		client.query(
+			'SELECT * from player WHERE email=$1',
+			[email],
+			callback
+		);
+	},
+
+	// add user maybe
+	function(prevResult, callback) {
+		if (prevResult.rows.length > 0) {
+			console.log('User exists; continuing...');
+			callback(null);
+		} else {
+			console.log('Adding user.');
+			query = client.query(
+				"INSERT INTO player (email) VALUES ($1)",
+				[email],
+				callback
+			);
+		}
+	},
+
+	// print results
+	function(callback) {
+		console.log('Getting all users.');
+		var query = client.query("SELECT * FROM player", callback);
+	}
+], function(err, result) {
+	// shut down client not matter what
+	client.end();
+
+	// check what we got
+	if (err) {
+		console.log('Problem:');
+		console.log(err);
+	} else {
+		console.log('Completed.');
+		for (var i = 0; i < result.rows.length; i++) {
+			console.log(result.rows[i]);
+		}
+	}
+});
+
+
+
