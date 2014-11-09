1a15f9531dfc85f32c2ac0ab98342e67f346b1a0
Max Forbes <maxforbes@google.com>
2014-11-08 18:24:08 -0800

    Add ORM notes. Undecided as to which to pick.

--- config/database.js
+++ config/database.js
@@ -2,6 +2,9 @@ module.exports = {
 	// Use this on Heroku.
 	// url: process.env.DATABASE_URL
 
-	// Use this for development.
+	// Use this for development (old Macbook pro)
 	'url': 'postgres://ludev1:1234@localhost:5432/ludb1'
+
+	// Use this for development (retina)
+	'url': 'postgres://TODO'
 };
